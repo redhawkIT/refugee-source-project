725d80db06a10b605d2439466917a9e92aa3a5c5
Ryan Keller <RyanKeller@my.UNT.edu>
2016-11-29 08:49:07 -0800

    Home has swipeable views.

--- src/Home.js
+++ src/Home.js
@@ -12,10 +12,15 @@ document:false, window:false, console:false, alert:false, user:false
 //    IMPORT DEPENDENCIES
 //    /////
 import React from 'react';
+import {Row, Col} from 'react-grid-system';
+
 
 //    /////
 //    MATERIAL-UI COMPONENTS
 //    /////
+import {Tabs, Tab} from 'material-ui/Tabs';
+import SwipeableViews from 'react-swipeable-views';
+import Paper from 'material-ui/Paper';
 import {Card, CardActions, CardHeader, CardMedia, CardTitle, CardText} from 'material-ui/Card';
 
 
@@ -30,6 +35,7 @@ const styles = {
 var Home = React.createClass ({
     getInitialState:function() {
         return({
+            slideIndex: 0,
             stateside: false,
             citizen: false,
             nationality: [''],  //Multiple nationalities.
@@ -51,17 +57,46 @@ var Home = React.createClass ({
         });
     },
     
+    handleSlide:function(value) {
+        this.setState({
+            slideIndex: value,
+        });
+    },
     
     render:function() {
         console.log('REFUGEE INFO:', this.state);
-        return (
-            <Card style={styles.card}>
-                <CardTitle title="Placeholder" subtitle="Home"/>
-                <CardText>
-                    Language: {this.props.lang}
-                </CardText>
-            </Card>
+        return (    
+            <div>
+                <Tabs
+                    onChange={this.handleSlide}
+                    value={this.state.slideIndex}
+                    >
+                    <Tab label="Map" value={0} />
+                    <Tab label="Directory" value={1} />
+                </Tabs>
+                <SwipeableViews
+                    index={this.state.slideIndex}
+                    onChangeIndex={this.handleSlide}
+                    >
+                    <Paper>
+                        <Card style={styles.card}>
+                            <CardTitle title="Home - Map"/>
+                            <CardText>
+                                Language: {this.props.lang}
+                            </CardText>
+                        </Card>
+                    </Paper>
+                    <Paper>
+                        <Card style={styles.card}>
+                            <CardTitle title="Home - Directory"/>
+                            <CardText>
+                                Language: {this.props.lang}
+                            </CardText>
+                        </Card>
+                    </Paper>
+                </SwipeableViews>   
             
+            </div>            
         );
     }
 });
