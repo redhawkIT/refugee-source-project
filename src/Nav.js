2076aaa4ee48906a8684d01ffd920e57ddd9c4fc
Ryan Keller <RyanKeller@my.UNT.edu>
2016-11-29 16:12:53 -0800

    Code cleaning. Now, I tried populating titles from firebase, but it causes huge issues...

--- src/Nav.js
+++ src/Nav.js
@@ -20,17 +20,11 @@ import ReactFireMixin from 'reactfire';
 //    MATERIAL-UI COMPONENTS
 //    /////
 import {List, ListItem} from 'material-ui/List';
-import Divider from 'material-ui/Divider';
 
 import {blueA400} from 'material-ui/styles/colors';
 
-//All the icons used in the nav (6)... yes, separate paths. :(
-import Face from 'material-ui/svg-icons/action/face';
-import VerifiedUser from 'material-ui/svg-icons/action/verified-user';
-import PermMedia from 'material-ui/svg-icons/action/perm-media';
-
-import MailOutline from 'material-ui/svg-icons/communication/mail-outline';
-import Call from 'material-ui/svg-icons/communication/call';
+import QuestionAnswer from 'material-ui/svg-icons/action/question-answer';
+import StoreMallDirectory from 'material-ui/svg-icons/maps/store-mall-directory';
 
 //    /////
 //    COMPONENT
@@ -38,32 +32,22 @@ import Call from 'material-ui/svg-icons/communication/call';
 const styles = {
     list: {
         overflowY: 'auto'
-    },
-    iconColor: blueA400
+    }
 };
 
+const iconColor = blueA400;
+
 var Nav = React.createClass ({
-    mixins: [ReactFireMixin],
-    getInitialState:function() {
-        return {
-            contact: ''
-        };
-    },
-    componentWillMount: function() {
-        var refContact = firebase.database().ref("contact");
-        this.bindAsObject(refContact, 'contact');
-    },
-    //Home, Resources, Services, About, Contribute
     render:function() {
         return (
             <List style={styles.list}>
                 <Link to="/">
                     <ListItem primaryText="Home"
-                        leftIcon={<Face color={styles.iconColor}/>}/>
+                        leftIcon={<StoreMallDirectory color={iconColor}/>}/>
                 </Link>
                 <Link to="/about">
                     <ListItem primaryText="About Us"
-                        leftIcon={<VerifiedUser color={styles.iconColor}/>} />
+                        leftIcon={<QuestionAnswer color={iconColor}/>} />
                 </Link>
             </List>
         );
